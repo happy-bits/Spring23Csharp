a43406b320c1a98b07b154a6f3389e0ee30d74a9
Oscar Olsson <happybits@gmail.com>
2023-02-23 15:35:04 +0100

    -

--- React/lab/src/email/Email.js
+++ React/lab/src/email/Email.js
@@ -2,10 +2,37 @@
 function Email() {
 
     return (
-        <>
-            <h1>En rubrik</h1>
-            <div>Jag är en email komponent!</div>
-        </>
+        
+
+
+                <article className="box">
+
+                    <h1 className="display-4 mb-4">Sign up</h1>
+
+                    <div className="mb-3">
+                        <label htmlFor="emailInput" className="form-label">Email address</label>
+                        <input type="email" className="form-control" id="emailInput" placeholder="name@example.com" />
+                    </div>
+
+                    <div className="mb-3">
+                        <label htmlFor="emailAgainInput" className="form-label">Enter again</label>
+                        <input type="email" className="form-control" id="emailAgainInput" placeholder="name@example.com" />
+                    </div>
+
+                    <button className="btn btn-primary btn-lg">Ok</button>
+
+                    <div className="alert alert-success mt-3" role="alert">
+                        Success message
+                    </div>
+
+                    <div className="alert alert-danger mt-3" role="alert">
+                        Error message
+                    </div>
+
+                </article>
+
+  
+  
     )
 
 
--- React/lab/src/index.js
+++ React/lab/src/index.js
@@ -4,6 +4,8 @@ import ReactDOM from 'react-dom/client';
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap.bundle.min";
 
+import './custom.css'
+
 import Email from './email/Email'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
@@ -11,12 +13,12 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
 
-    <h1>Hej hej</h1>
+    <div className="container mt-3">
 
-    <Email />
-    <Email />
+      <Email />
+      <Email />
 
-    <p>Lite text</p>
+    </div>
 
   </React.StrictMode>
 );
