ed0eb7f4eda3b67965f5cf72387d31feeb20250c
Oscar Olsson <happybits@gmail.com>
2023-02-23 16:17:59 +0100

    -

---
+++ React/lab/src/email/Email03.js
@@ -0,0 +1,50 @@
+
+
+import { useState } from "react"
+
+function Email() {
+
+    const [input, setInput] = useState("oo@happybits.se")
+
+    const changeEmailAddress = () => {
+
+        setInput("klas@katt.nu")
+    }
+    
+    const inputChanged = (event)=> {
+        setInput(event.target.value)
+    }
+    
+    return (
+
+        <div className="center">
+
+            <article className="box">
+
+                <h1>Sign up</h1>
+
+
+                <div className="mb-3">
+                    <label htmlFor="emailInput" className="form-label">Email address</label>
+                    <input value={input} onChange={inputChanged}  type="email" className="form-control" id="emailInput" placeholder="name@example.com" />
+                </div>
+
+                <div className="alert alert-primary">
+                    {input}
+                </div>
+
+                <div className="mb-3">
+                    <label htmlFor="emailAgainInput" className="form-label">Enter again</label>
+                    <input type="email" className="form-control" id="emailAgainInput" placeholder="name@example.com" />
+                </div>
+
+                <button onClick={changeEmailAddress} className="btn btn-primary btn-lg me-1">Ok1</button>
+
+            </article>
+
+        </div>
+
+    )
+}
+
+export default Email
