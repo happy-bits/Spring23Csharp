b564640625a50629c736960ea0ad1783067eddd4
Oscar Olsson <happybits@gmail.com>
2023-02-23 19:17:55 +0100

    -

--- React/lab/src/email/Email03.js
+++ React/lab/src/email/Email03.js
@@ -1,48 +1,81 @@
-
-
-import { useState } from "react"
+/*
+    Keep track on what the user writes in the textbox (this is saved in "state")
+   
+    The GUI is automatically updated when the state is changed
+*/
+import { useState } from 'react'
 
 function Email() {
 
-    const [input, setInput] = useState("oo@happybits.se")
+    const [input, setInput] = useState("")
 
     const changeEmailAddress = () => {
 
+        // input = "klas@katt.nu"
+
         setInput("klas@katt.nu")
+
     }
-    
-    const inputChanged = (event)=> {
+
+    /*
+    When you want to change a state, don't write
+
+        input = "aaa"
+        
+    ...instead write
+
+        setInput("aaa")
+
+    */
+
+    const inputChange = (event) => {
+        /*
+           event.target =>       that's "input"
+           event.target.value => that's the entered value in the box
+
+           Let's change "input" to the value in the box       
+
+           Think
+           input = event.target.value
+
+           but write
+           setInput(event.target.value)
+
+        */
         setInput(event.target.value)
+
+        /*
+        React will now automagically update the component!
+        */
     }
-    
+
     return (
 
-        <div className="center">
 
-            <article className="box">
+        <article className="box">
 
-                <h1>Sign up</h1>
+            <h1>Sign up</h1>
+
+            <div className="mb-3">
+                <label htmlFor="emailInput" className="form-label">Email address</label>
+                <input type="email" value={input} onChange={inputChange} className="form-control" id="emailInput" placeholder="name@example.com" />
+            </div>
+
+            <div class="alert alert-primary" role="alert">
+                {input}
+            </div>
 
 
-                <div className="mb-3">
-                    <label htmlFor="emailInput" className="form-label">Email address</label>
-                    <input value={input} onChange={inputChanged}  type="email" className="form-control" id="emailInput" placeholder="name@example.com" />
-                </div>
+            <div className="mb-3">
+                <label htmlFor="emailAgainInput" className="form-label">Enter again</label>
+                <input type="email" className="form-control" id="emailAgainInput" placeholder="name@example.com" />
+            </div>
 
-                <div className="alert alert-primary">
-                    {input}
-                </div>
+            <button onClick={changeEmailAddress} className="btn btn-primary btn-lg">Change email address</button>
 
-                <div className="mb-3">
-                    <label htmlFor="emailAgainInput" className="form-label">Enter again</label>
-                    <input type="email" className="form-control" id="emailAgainInput" placeholder="name@example.com" />
-                </div>
+        </article>
 
-                <button onClick={changeEmailAddress} className="btn btn-primary btn-lg me-1">Ok1</button>
 
-            </article>
-
-        </div>
 
     )
 }
--- React/lab/src/email/Email04.js
+++ React/lab/src/email/Email04.js
@@ -1,73 +1,67 @@
+/*
+    Keep track of the value of both textfields
 
-
-import { useState } from "react"
+    Write a text if the values are the same
+*/
+import { useState } from 'react'
 
 function Email() {
 
     const [input, setInput] = useState("")
     const [inputAgain, setInputAgain] = useState("")
 
-    const changeEmailAddress = () => {
-        setInput("klas@katt.nu")
-    }
-    
-    const inputChanged = (event)=> {
+    const inputChange = (event) => {
+
         setInput(event.target.value)
     }
-    
-    const inputAgainChanged = (event)=> {
+
+    const inputAgainChange = (event) => {
+
         setInputAgain(event.target.value)
     }
 
-    const compareText = () => {
-
-        // Om input är samma som inputAgain, då vill jag returnera "Same", annars "Not the same"
-
-        if (input === inputAgain){
+    const compareTexts = () => {
+        if (input === inputAgain) {
             return "Same"
         } else {
             return "Not the same"
         }
-
     }
 
     return (
 
-        <div className="center">
 
-            <article className="box">
+        <article className="box">
 
-                <h1>Sign up</h1>
+            <h1>Sign up</h1>
 
+            <div className="mb-3">
+                <label htmlFor="emailInput" className="form-label">Email address</label>
+                <input type="email" value={input} onChange={inputChange} className="form-control" id="emailInput" placeholder="name@example.com" />
+            </div>
 
-                <div className="mb-3">
-                    <label htmlFor="emailInput" className="form-label">Email address</label>
-                    <input value={input} onChange={inputChanged}  type="email" className="form-control" id="emailInput" placeholder="name@example.com" />
-                </div>
+            <div class="alert alert-primary" role="alert">
+                {input}
+            </div>
 
-                <div className="alert alert-primary">
-                    {input}
-                </div>
+            <div className="mb-3">
+                <label htmlFor="emailAgainInput" className="form-label">Enter again</label>
+                <input type="email" value={inputAgain} onChange={inputAgainChange} className="form-control" id="emailAgainInput" placeholder="name@example.com" />
+            </div>
 
-                <div className="mb-3">
-                    <label htmlFor="emailAgainInput" className="form-label">Enter again</label>
-                    <input value={inputAgain} onChange={inputAgainChanged} type="email" className="form-control" id="emailAgainInput" placeholder="name@example.com" />
-                </div>
+            <div class="alert alert-primary" role="alert">
+                {inputAgain}
+            </div>
 
-                <div className="alert alert-primary">
-                    {inputAgain}
-                </div>
+            <button className="btn btn-primary btn-lg">Ok</button>
 
-                <button onClick={changeEmailAddress} className="btn btn-primary btn-lg me-1">Ok1</button>
+            <div class="alert alert-primary mt-3" role="alert">
+                <div>{compareTexts()}</div>
+                <div>{input === inputAgain ? "Same" : "Not the same"}</div>
+            </div>
 
-                <div className="alert alert-primary mt-3">
-                    <div>{compareText()}</div>
-                    <div>{input === inputAgain ? "Same!" : "Not the same!"}</div>
-                </div>
+        </article>
 
-            </article>
-
-        </div>
 
     )
 }
