f86b3d7d73a19294d19a2535658cdfe475e07276
Oscar Olsson <happybits@gmail.com>
2023-02-24 16:49:27 +0100

    -

--- React/lab/src/index.js
+++ React/lab/src/index.js
@@ -1,3 +1,5 @@
+/* eslint no-unused-vars: "off" */ 
+
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 
@@ -7,6 +9,10 @@ import "bootstrap/dist/js/bootstrap.bundle.min";
 import './custom.css'
 
 import Email from './email/Email'
+import Movie from './movie/Movie'
+
+import goodBadUgly from './movie/posters/good-bad-ugly.jpg'
+import ghostbusters from './movie/posters/ghostbusters.jpg'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
@@ -16,11 +22,11 @@ root.render(
     <div className="container mt-3">
 
       <div className="my-5">
-      <Email success-message="Good work!" background="cornsilk"  border-thickness={10} />
+      <Movie title="Ghostbusters" poster={ghostbusters} maxstars={7} />
       </div>
 
       <div className="my-5">
-        <Email />
+      <Movie />
       </div>
 
     </div>
