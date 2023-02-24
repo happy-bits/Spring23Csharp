3857847af48a6b9dd0b6ba0a86f47114de9e203e
Oscar Olsson <happybits@gmail.com>
2023-02-24 16:23:53 +0100

    -

---
+++ React/lab/src/movie/Movie07.js
@@ -0,0 +1,80 @@
+/*
+
+Write how many stars that were chosen: "You rated X stars"
+
+*/
+
+import { useState } from 'react'
+import defaultPoster from './posters/default.png'
+import starGray from './stars/star-gray.png'
+import star from './stars/star.png'
+
+function Movie(param) {
+
+    const [chosenStars, setChosenStars] = useState(0)
+    const [hoveredStar, setHoveredStar] = useState(null)
+
+    const maxstars = param.maxstars ?? 5
+    const title = param.title ?? "<No name>"
+    const poster = param.poster ?? defaultPoster
+
+    const clickStar = (number)=>{
+        setChosenStars(number)
+    }
+
+    const hoverStar = (number)=>{
+        setHoveredStar(number)
+    }
+
+    const leftStar = () => {
+        setHoveredStar(null)
+    }
+
+    const selectStarImageFor = (number) => {
+        if (hoveredStar!=null){
+
+            if (hoveredStar >= number){
+                return star
+            } else {
+                return starGray
+            }    
+        }
+
+        if (chosenStars >= number){
+            return star
+        } else {
+            return starGray
+        }
+    }
+
+    return (
+
+        <article className="box movie">
+
+            <h2 className='text-center'>{title}</h2>
+
+            <div className="text-center">
+                <img className="img-fluid" src={poster} />
+            </div>
+
+            <div className="text-center">
+                
+                {[1, 2, 3, 4, 5].map(n=>
+                    <img key={n} src={selectStarImageFor(n)} onClick={() => clickStar(n)} onMouseEnter={() => hoverStar(n)} onMouseOut={() => leftStar()} className='movie__star' />
+                )}
+
+            </div>
+
+            <div className="movie__info" onClick={()=>clickStar(0)}>
+
+            { chosenStars === 0 ? <>&nbsp;</> : <>You rated {chosenStars} stars!</> }
+            
+            </div>
+
+        </article>
+
+    )
+}
+
+export default Movie
+
